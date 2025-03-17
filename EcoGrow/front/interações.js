20476229f2ef9ef383011993d534afb196771626
Pabloenteno <136346954+Pabloenteno@users.noreply.github.com>
2025-03-17 14:07:43 -0300

    Add files via upload

---
+++ EcoGrow/front/interações.js
@@ -0,0 +1,5 @@
+const irLogin = document.getElementById('irLogin').addEventListener('click' ,function() { window.location = 'login.html' });
+const iCadastrar = document.getElementById('irCadastrar').addEventListener('click' ,function() { window.location = 'cadastro.html' });
+const voltar = document.getElementById('voltar').addEventListener('click' ,function() { window.location = 'pgPrincipal.html' });
+
+const logarCadastrar = document.getElementById('irPergunta').addEventListener('click' ,function() { window.location = 'pergunta.html' });
